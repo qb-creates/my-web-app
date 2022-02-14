b275d093f0ee22c9116a537a799199807877c63e
Quentin Baker <qbakernc@gmail.com>
2022-02-13 22:48:19 -0500

    Updated

--- src/app/affirmation/affirmation.component.ts
+++ src/app/affirmation/affirmation.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-affirmation',
   templateUrl: './affirmation.component.html',
   styleUrls: ['./affirmation.component.css']
 })
-export class AffirmationComponent implements OnInit {
+export class AffirmationComponent {
+  @Output() affirmationOutput!: string;
 
-  constructor() { }
+  randPrevious:number = 0;
+  affirmation:string[] = [
+    "The grind dont stop. I see you are putting the work. You got this",
+    "Hello future millionaire. Get up and get to it. You are going achieve everything you dreamed of.",
+    "If most people worked half as hard as you do, they would be successfull af.",
+    "Don't listen to the negativity. You know what you are capable of. Grind Grind Grind",
+    "Winnie is lucky to have such a hard working mother. You will be a great example for her."
+  ];
 
-  ngOnInit() {
+  constructor() 
+  { 
+    let rand = Math.floor(Math.random() * (1 + 4  - 0) + 0);
+    this.affirmationOutput = this.affirmation[rand];  
   }
 
+  generate() {
+    let rand = Math.floor(Math.random() * (1 + 4  - 0) + 0);
+    this.affirmationOutput = this.affirmation[rand];  
+  }
 }
